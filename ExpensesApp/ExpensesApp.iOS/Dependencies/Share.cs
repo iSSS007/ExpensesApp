11a4cc4dcd3e23d04ac5e6ef3e10e2f2dcc244f3
Saurabh Shivgan <saurabh@digitalinfobytes.com>
2024-03-05 11:02:48 +0530

    Add Export functionality for iOS

--- ExpensesApp/ExpensesApp.iOS/Dependencies/Share.cs
+++ ExpensesApp/ExpensesApp.iOS/Dependencies/Share.cs
@@ -16,6 +16,23 @@ namespace ExpensesApp.iOS.Dependencies
         {
         public async Task Show(string title, string message, string filePath)
             {
+            var viewController = GetVisibleViewController();
+            var items = new NSObject[] {NSObject.FromObject(title), NSUrl.FromFilename(filePath) };
+            var activityController = new UIActivityViewController(items, null);
+            if(activityController.PopoverPresentationController != null)
+activityController.PopoverPresentationController.SourceView=viewController.View;
+            await viewController.PresentViewControllerAsync(activityController,true);
+            }
+        private UIViewController GetVisibleViewController()
+            {
+            var rootViewController = UIApplication.SharedApplication.KeyWindow.RootViewController;
+            if (rootViewController.PresentedViewController == null)
+                return rootViewController;
+            if(rootViewController.PresentedViewController is UINavigationController)
+                return ((UINavigationController)rootViewController.PresentedViewController).TopViewController;
+            if (rootViewController.PresentedViewController is UITabBarController)
+                return ((UITabBarController)rootViewController.PresentedViewController).SelectedViewController;
+            return rootViewController.PresentedViewController;
             }
         }
     }
